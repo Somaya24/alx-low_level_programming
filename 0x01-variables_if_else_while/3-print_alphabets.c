aa95c46234011bf643bda04c34425f286b8841a8
Soumia <soumiae4@gmail.com>
2023-06-16 16:35:38 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
 
 /**
- * main: Entry point
+ * main - Entry point
+ *
+ * Description: print the alphabets
  *
  * Return: Always 0 (Success)
- *
  */
+
 int main(void)
 {
-char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-int i;
-for (i = 0; i < 52; i++)
-{
-	putchar(alp[i]);
-}
-putchar('\n');
-return (0);
+	char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
+
+	for (i = 0; i < 52; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+	return (0);
 }

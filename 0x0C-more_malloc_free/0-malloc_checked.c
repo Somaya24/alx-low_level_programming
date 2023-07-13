b1b0a1e8e72d725eccece64c0da1cc97a0eaf305
Soumia <soumiae4@gmail.com>
2023-07-13 10:23:49 -0700

    malloc checked

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+/**
+ * main - check the code
+ *
+ *Return: Always 0
+ */
+int main(void)
+{
+	int *n;
+
+	n = malloc_checked(98);
+	printf("%p\n", (void *)n);
+	free(n);
+	return (0);
+}

26063d48a4cd35d1b02b255635e122127ed57691
Soumia <soumiae4@gmail.com>
2023-08-01 09:19:44 -0700

    print listint

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,8 +1,8 @@
 #include "lists.h"
 
 /**
- * print_listint: prints all the elements of a linked list
- * @h: pointer to the list_t list to print
+ * print_listint - prints all the elements of a linked list
+ * @h: linked list of type listint_t list to print
  *
  * Return: the number of nodes printed
  */

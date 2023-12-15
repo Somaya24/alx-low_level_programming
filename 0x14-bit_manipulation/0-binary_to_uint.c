1e5cf956a87231f1169d767fab61b0392b54804a
Soumia <soumiae4@gmail.com>
2023-12-15 14:28:49 -0800

    betty

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,7 +9,7 @@ unsigned int binary_to_uint(const char *b)
 	if (b == NULL)
 		return (0);
 
-	for(i = (blen - 1); i >= 0; i--)
+	for (i = (blen - 1); i >= 0; i--)
 	{
 		if (b[i] == '1')
 			total = total + decimal;

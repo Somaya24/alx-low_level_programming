44d3e1646f76738ca1509816e9f8123fceb064b6
Soumia <soumiae4@gmail.com>
2023-12-15 14:26:09 -0800

    first task

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int total = 0;
+	int blen = strlen(b);
+	int decimal = 1, i;
+
+	if (b == NULL)
+		return (0);
+
+	for(i = (blen - 1); i >= 0; i--)
+	{
+		if (b[i] == '1')
+			total = total + decimal;
+		decimal = decimal * 2;
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+	}
+	return (total);
+}
